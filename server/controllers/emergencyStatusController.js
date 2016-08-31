d489a21034da4164f32cdf3374f72ea672bb4b8b
M0ss Man <merikwood@gmail.com>
2016-08-31 14:34:57 -0700

    [REFACTOR] rebuilt push notifications

--- server/controllers/emergencyStatusController.js
+++ server/controllers/emergencyStatusController.js
@@ -1,11 +1,34 @@
 var mongoose = require('mongoose');
 var Organization = require('../db/schema.js')['Organization'];
+var Employee = require('../db/schema.js')['Employee'];
+var apple = require('../helpers/applepushHelper.js');
 
 module.exports = {
   "PUT": function(req, res){
     console.log(req.user);
     Organization.update({ orgName: req.user.orgName}, { inEmergency: true})
-    .then(function(result){
+    .then(function(){
+      return Employee.find({orgName: req.user.orgName}, '-_id deviceToken')
+    })
+    .then(function(data){
+      console.log("employeeArray", data);
+      var justDeviceTokenArr = data.filter(function(element){
+        console.log(element['deviceToken'] === "");
+        return element['deviceToken'] !== "";
+      }).map(function(entry){
+        return apple.createDevice(entry['deviceToken']);
+      })
+      console.log(justDeviceTokenArr, "just token array")
+      console.log("about to send to apple for push");
+
+      var note = apple.createNote();
+      note.expiry = Math.floor(Date.now() / 1000) + 3600;
+      note.badge = 1;
+      note.sound = "ping.aiff";
+      note.alert = "EMERGENCY ALERT";
+      note.payload = { "EMERGENCY": "There is a fire in the building please evacuate immediately"};
+      apple.connection.pushNotification(note, justDeviceTokenArr);
+
       res.status(200).send("Organization Emergency Status updated");
     })
     .catch(function(err){
--- server/controllers/employeeStatusController.js
+++ server/controllers/employeeStatusController.js
@@ -1,10 +1,12 @@
 var mongoose = require('mongoose');
+var apple = require('../helpers/applepushHelper.js');
 var Employee = require('../db/schema.js')['Employee'];
+
 
 module.exports = {
   "GET": function(req, res){
     console.log(req.user);
-    Employee.find({ orgName: req.user.orgName}, '-_id name status wardenName')
+    Employee.find({ orgName: req.user.orgName, wardenName: req.user.wardenName}, ' name status wardenName')
       .then(function(result){
         console.log('result in get of employeeStatus', result);
         res.status(200).send(result);
@@ -18,7 +20,25 @@ module.exports = {
     Employee.update({ userName: req.user.userName}, { status: req.body.status})
       .then(function(){
         console.log('employeeStatus updated successfully');
-        res.status(200).send('employeeStatus updated successfully');
+        return Employee.find({ orgName: req.user.orgName, wardenName: req.user.wardenName}, '-_id deviceToken');
+      })
+      .then(function(data){
+        console.log(data);
+        var justDeviceTokenArr = data.filter(function(element){
+          return element['deviceToken'] !== "";
+        }).map(function(entry){
+          return apple.createDevice(entry['deviceToken']);
+        })
+        console.log(justDeviceTokenArr, "just token array")
+        var note = apple.createNote();
+        note.expiry = Math.floor(Date.now() / 1000) + 3600;
+        // note.badge = 1;
+        // note.sound = "ping.aiff";
+        // note.alert = "EMERGENCY ALERT";
+        note.payload = { "userStatus": req.body.status, "userStatusUpdate": true, "silent": true};
+        note['content-available'] = 1;
+        apple.connection.pushNotification(note, justDeviceTokenArr);
+        res.status(200).send('push notifications sent successfully');
       })
       .catch(function(err){
         console.log('error in employeeStatus put', err);
